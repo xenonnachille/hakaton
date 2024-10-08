d1baf540d04b4f6b59a2f315fca1613760c05acc
xenonnachille <sheviakova.kv@phystech.edu>
2024-10-08 20:16:43 +0300

    почнему все так

---
+++ mysite/main/templates/main/model.py
@@ -0,0 +1,10 @@
+import sys
+
+def main():
+    input_string = sys.argv[1]
+    # Здесь выполняйте необходимые действия с input_string
+    output_string = f"Вы передали строку: {input_string}"
+    
+    print(output_string)
+
+main()
